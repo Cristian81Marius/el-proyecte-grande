835422ba136fc45a7eaba081143fb010236f8403
Marius_Cristian <cry.ichimas81@gmail.com>
2023-02-03 01:03:43 +0200

    adding payment route

--- el-proyecte-grande/src/Pages/Buy.js
+++ el-proyecte-grande/src/Pages/Buy.js
@@ -1,99 +1,29 @@
-import { useState } from "react";
-import Card from "../components/Aspect/Card";
-import { Form } from "semantic-ui-react";
+// import { useState } from "react";
+// import Card from "../components/Aspect/Card";
+// import { Form } from "semantic-ui-react";
+import axios from "../Pages/FetchRequest/axios"
 
 function Buy(){
-    window.location.replace("https://buy.stripe.com/test_eVaaGFenubFl14QfYZ")
-    // const [number, setnumber] = useState("");
-    // const [date, setDate] = useState("");
-    // const [cvv, setCvv] = useState("");
-
-    // const data={
-    //     number : number,
-    //     date : date,
-    //     cvv : cvv
-    // }
-
-    return<>
-    <form action="/Payment" method="POST">
-      <button type="submit">Pay 120 lei</button>
-    </form>
-    </> 
-    // <Card>
-        {/* <form>
-            <label hrmlfor="number">Credic card number</label>
-            <input value={number} onChange={(e)=> setnumber(e.target.value)} id="number" name="number"></input>
-            <label hrmlfor="expyration">Expire Date</label>
-            <input value={date} onChange={(e)=> setDate(e.target.value)} type= "date" id="date" name="date"></input>
-            <label hrmlfor="cvv">CVV</label>
-            <input value={cvv} onChange={(e)=> setCvv(e.target.value)} id="cvv" name="cvv"></input>
-            <button type="submit">Log In</button>
-        </form>
- */}
-
-
-    {/* <form class="ui form">
-        <h4 class="ui dividing header">Billing Information</h4>
-        <div class="field">
-        <label>Card Type</label>
-            <div class="ui selection dropdown">
-            <input type="hidden" name="card[type]"/>
-            <div class="default text">Type</div>
-            <i class="dropdown icon"></i>
-      <div class="menu">
-        <div class="item" data-value="visa">
-          <i class="visa icon"></i>
-          Visa
-        </div>
-        <div class="item" data-value="amex">
-          <i class="amex icon"></i>
-          American Express
-        </div>
-        <div class="item" data-value="discover">
-          <i class="discover icon"></i>
-          Discover
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="fields">
-    <div class="seven wide field">
-      <label>Card Number</label>
-      <input type="text" name="card[number]" maxlength="16" placeholder="Card #"/>
-    </div>
-    <div class="three wide field">
-      <label>CVC</label>
-      <input type="text" name="card[cvc]" maxlength="3" placeholder="CVC"/>
-    </div>
-    <div class="six wide field">
-      <label>Expiration</label>
-      <div class="two fields">
-        <div class="field">
-          <select class="ui fluid search dropdown" name="card[expire-month]">
-            <option value="">Month</option>
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-          </select>
-        </div>
-        <div class="field">
-          <input type="text" name="card[expire-year]" maxlength="4" placeholder="Year"/>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="ui button" tabindex="0">Submit</div>
-</form> */}
-        {/* </Card> */}
+    // window.location.replace("https://buy.stripe.com/test_eVaaGFenubFl14QfYZ")
+    const  a = async() =>{
+      const product= {
+        id: "string",
+        title: "string",
+        description: "string",
+        imageUrl: "string",
+        price: 1
+      }
+  
+      await axios.post('payment'
+      ).then(function (response) {
+        }).catch(function (error) {
+          console.log(error);
+        })
+  }
+  console.log("a")
+      return<>
+        {/* <button type="submit" onClick={onBuyHandler}>Pay 120 lei</button> */}
+{a()}      
+</> 
 }
-
 export default Buy;
--- el-proyecte-grande/src/Pages/Home.js
+++ el-proyecte-grande/src/Pages/Home.js
@@ -1,15 +1,19 @@
 import CommonPage from "./CommonPage";
 import { Link } from "react-router-dom";
 import {Button} from "semantic-ui-react";
+import Buy from "./Buy";
 
 function Home(){
-    function onClickHendler(){
-        window.location.replace("/buy")
-    }
+
     return <div>
         <CommonPage/>
 
-        <Button primary  onClick={onClickHendler}> Buy </Button>
+        <form action="http://localhost:50864/api/payment" method="POST">
+      <button type="submit">Checkout</button>
+    </form>
+        {/* <Button primary  onClick={Buy}> Buy </Button> */}
+        {/* <Button primary  onClick={() =>window.location.replace("https://buy.stripe.com/test_cN2dSR4MU4cTaFq5kk") }> Buy </Button> */}
+
         Cumpara pachetul <Link to="/buy"><br/> 1Leu</Link>
 
         <Link to="/getToken"><br/> Token for password</Link>
